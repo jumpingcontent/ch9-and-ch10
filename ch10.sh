5c99768da9af55ac92962f358d83de44af7f0f97
Ron <35547141+jumpingcontent@users.noreply.github.com>
2018-03-28 17:36:55 -0500

    Create ch10.sh

---
+++ ch10.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# Telnet command to connect to google's main domain server
+telnet www.google.com 80
+
+# Making a singular connection for a resource request
+Get / HTTP/1.0
+
+# Listing of programs using or listening to ports
+losf -i
+
+# To see output of TCP web and UDP packets
+tcpdump udp or port 80
+
+# See which RPC services my computer has
+rpcinfo -p localhost
+
